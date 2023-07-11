be2dc53a8d1d4b811c17e9e1f243ee216ac248a9
Giancarlo <giancarlo.dago@unina.it>
2023-07-11 15:41:31 +0200

    Minor changes

--- cable-suspended-robots-ros-pkg/include/ArmsInducedOscillationCranebot.hpp
+++ cable-suspended-robots-ros-pkg/include/ArmsInducedOscillationCranebot.hpp
@@ -1,15 +1,11 @@
 #include "ros/ros.h"
-// #include "boost/thread.hpp"
 #include "std_msgs/Float64.h"
-// #include "std_srvs/Empty.h"
 #include "sensor_msgs/JointState.h"
 #include "gazebo_msgs/LinkStates.h"
 #include "gazebo_msgs/SetModelConfiguration.h"
 #include "rosgraph_msgs/Clock.h"
 #include <string>
 #include <iostream>
-// #include <fstream>
-// #include <stack>
 #include <thread>
 #include <chrono>
 #include <eigen3/Eigen/Geometry>
