657230668d80aee32d4e8cf42c807b77ae0ebe9b
Giancarlo <giancarlo.dago@unina.it>
2023-07-12 16:32:05 +0200

    Class definition moved

--- cable-suspended-robots-ros-pkg/src/ArmsInducedOscillationCranebot.cpp
+++ cable-suspended-robots-ros-pkg/src/ArmsInducedOscillationCranebot.cpp
@@ -1,27 +1,5 @@
 #include "ArmsInducedOscillationCranebot.hpp"
 
-class ArmsInducedOscillation {
-    private:
-        ros::NodeHandle nh_;
-        ros::Subscriber linkStateSub_;
-        ros::Subscriber gazeboClockSub_;
-        ros::Publisher j1APosPub_, j2APosPub_, j3APosPub_, j4APosPub_, j5APosPub_, j6APosPub_, \
-                       j1BPosPub_, j2BPosPub_, j3BPosPub_, j4BPosPub_, j5BPosPub_, j6BPosPub_;
-        ros::ServiceClient setModelConfigurationClient_;
-        gazebo_msgs::SetModelConfiguration configSrv_;
-        double platformLinkPose_[7];
-        double gazeboTime_;
-        double resetWaitTime_;
-    public:
-        ArmsInducedOscillation();
-        void run();
-        void generateOscillation();
-        void linkStateCB(gazebo_msgs::LinkStates);          // Link state callback
-        void gazeboClockCB(rosgraph_msgs::Clock);           // Gazebo clock callback
-};
-
-
-
 // Constructor
 ArmsInducedOscillation::ArmsInducedOscillation() {                                         // Unpause Gazebo Client
     setModelConfigurationClient_ = nh_.serviceClient<gazebo_msgs::SetModelConfiguration>("/gazebo/set_model_configuration");        // Set Model Configuration Client
